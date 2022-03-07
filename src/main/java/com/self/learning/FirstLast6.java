e23905456cf03295f85162c98e2d47f3d72d2de1
chukkakrishna <venkatsai.chukka1@gmail.com>
2022-03-07 23:03:52 +0530

    FirstLast6 solution

--- src/main/java/com/self/learning/FirstLast6.java
+++ src/main/java/com/self/learning/FirstLast6.java
@@ -6,7 +6,7 @@ firstLast6([6, 1, 2, 3]) → true
 firstLast6([13, 6, 1, 2, 3]) → false
  */
 public class FirstLast6 {
-    public boolean fIRSTlASTPGM(int[] a){
+    public boolean firstLastPgm(int[] a){
         /*
         for (int i=0;i<a.length;i++){
             System.out.println("values are "+a[i]);
@@ -20,6 +20,6 @@ public class FirstLast6 {
     }
     public static void main(String[] args) {
         FirstLast6 obj1= new FirstLast6();
-        System.out.println(obj1.fIRSTlASTPGM(new int[]{6,13, 6, 1, 2, 3}));
+        System.out.println(obj1.firstLastPgm(new int[]{6,13, 6, 1, 2, 3}));
     }
 }
